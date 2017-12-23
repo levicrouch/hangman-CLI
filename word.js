a3e0893032d9b06fa239773c52d7c99d587cb433
Levi Crouch <levi.crouch@gmail.com>
2017-12-22 23:31:33 -0800

    letter count is now case insensitive

--- word.js
+++ word.js
@@ -2,7 +2,7 @@
 
 // DONE grab a movie title from array
 
-// Count the number of characters in the title
+// DONE Count the number of characters in the title
 
 // display a few hint's from the movie data after each incorrect guess
 
@@ -14,7 +14,7 @@ var Word = function () {
         "The Usual Suspects", "Eternal Sunshine of the Spotless Mind", "The Green Mile", "Requiem for a Dream", "Fight Club",
         "Memento", "The Pianist", "Schindler's List", "Confessions", "Inglourious Basterds", "Django Unchained",
         "Kill Bill: Vol. 1", "Gattaca", "Secretary", "Lars and the Real Girl", "In Bruges", "Blue Valentine", "Submarine",
-        "The Talented Mr. Ripley", "An Education", "Big Fish", "Zindagi Na Milegi Dobara", "Barfi!", "The Departed",
+        "The Talented Mr. Ripley", "An Education", "Big Fish", "Zindagi Na Milegi Dobara", "Barfi", "The Departed",
         "Saving Private Ryan", "Gladiator", "Garden State", "Shutter Island", "Drive", "Being Flynn", "A Beautiful Mind",
         "Black Swan", "Inception", "Silver Linings Playbook", "A Clockwork Orange"
     ];
@@ -25,22 +25,18 @@ var Word = function () {
         return this.storedPick;
     };
     this.letterCount = function (word) {
-        // take the title and convert it to lowerCase
-        var word = word.toLowerCase();
-        var regEx = /[a-z]/gi;
+        // regex to match all the letters of a title (case-insensitive)
+        var regEx = /[aA-zZ]/gi;
         var match = word.match(regEx);
         if (match) {
             this.storedLetterCount = match.length;
         }
-        // console.log("match.length",match.length)
-        return match.length;
+        return this.storedLetterCount;
     };
     this.storedPick = "";
     this.storedLetterCount = "";
 };
 var objNewPick = new Word();
-// var newWord = newPick.pick();
-// var newWord = newPick.letterCount(newWord);
 for (i = 0; i < objNewPick.movieTitleArr.length; i++) {
     console.log(objNewPick.pick());
     console.log("Number of letters in selected word: " + objNewPick.letterCount(objNewPick.storedPick));
