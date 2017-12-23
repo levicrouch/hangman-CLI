58dd0c2baa3249afffef5e63b0761b91e9900ea1
Levi Crouch <levi.crouch@gmail.com>
2017-12-23 15:10:24 -0800

    basic functionality to disguise the selected word and to reveal a character if it matches a letter in the selected word

--- word.js
+++ word.js
@@ -1,13 +1,14 @@
 // constructor to generate and display the words for the game
-
-// DONE grab a movie title from array
-
-// DONE Count the number of characters in the title
+// Add inquirer
+var inquirer = require('inquirer');
 
 // display a few hint's from the movie data after each incorrect guess
 
-// Create a counter to track which question's have been asked
+// display underscore character under each letter to represent the letters of that word
+
+// DONE Create a counter to track which question's have been asked
 var count = 0
+var userGuessArray = [];
 var Word = function () {
     this.movieTitleArr = [
         "The Shawshank Redemption", "Good Will Hunting", "Forrest Gump", "Pulp Fiction", "American Beauty",
@@ -19,12 +20,17 @@ var Word = function () {
         "Black Swan", "Inception", "Silver Linings Playbook", "A Clockwork Orange"
     ];
     this.pick = function () {
+        // DONE grab a movie title from array
         // pick a title from the movieTitleArr to display to the user
         this.storedPick = this.movieTitleArr[count];
         count++;
+        // output the selected word - disguised
+        this.disguisedWord = this.storedPick.replace(/[aA-zZ]/gi, "?");
+        console.log("disguisedWord", this.disguisedWord);
         return this.storedPick;
     };
     this.letterCount = function (word) {
+        // DONE Count the number of characters in the title
         // regex to match all the letters of a title (case-insensitive)
         var regEx = /[aA-zZ]/gi;
         var match = word.match(regEx);
@@ -33,12 +39,46 @@ var Word = function () {
         }
         return this.storedLetterCount;
     };
+    // Store the picked word and letter count of that word
     this.storedPick = "";
     this.storedLetterCount = "";
+    this.disguisedWord = "";
+    this.SetCharAt = function (str, index, chr) {
+        if (index > str.length - 1) return str;
+        return str.substr(0, index) + chr + str.substr(index + 1);
+    }
+    this.displayLetters = function (selectedWord, chosenLetter) {
+        // convert word to lowercase
+        selectedWord = selectedWord.toLowerCase();
+        // Create an array to store the index of the matched letter
+        var matchIndex = [], i;
+        // replace each character with a question mark
+        // disguisedWord = selectedWord.replace(/[aA-zZ]/gi, "?");
+        console.log("this.disguisedWord", this.disguisedWord);
+        // search through the selectedWord string like an array
+        for (i = 0; i < selectedWord.length; i++) {
+
+            // if the chosenLetter matches a letter in the selectedWord...
+            if (selectedWord[i] === chosenLetter) {
+                // push that index into the matchIndexArray
+                matchIndex.push(i);
+                // console.log("matchIndex", matchIndex);
+                this.disguisedWord = this.disguisedWord.substr(0, i) + chosenLetter + this.disguisedWord.substr(i + 1);
+                
+            }
+        }
+        console.log("this.disguisedWord", this.disguisedWord);
+
+    };
 };
 var objNewPick = new Word();
+objNewPick.pick();
+objNewPick.displayLetters(objNewPick.storedPick, "s");
 for (i = 0; i < objNewPick.movieTitleArr.length; i++) {
-    console.log(objNewPick.pick());
-    console.log("Number of letters in selected word: " + objNewPick.letterCount(objNewPick.storedPick));
-    // console.log(objNewPick.letterCount(objNewPick.pick()));
+    // console.log(objNewPick.pick());
+    // console.log("Number of letters in selected word: " + objNewPick.letterCount(objNewPick.storedPick));
+    // Or simply write output
+
+    // ui.updateBottomBar('new bottom bar content');
 }
+module.exports = Word;
